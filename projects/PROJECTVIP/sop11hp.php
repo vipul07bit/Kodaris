714eae4df5c8e096edbff321af76465ae1b0e250
vipul07bit <officialvipul0622.yt@gmail.com>
2025-05-24 12:49:58 +0530

    Add files via upload

---
+++ projects/PROJECTVIP/sop11hp.php
@@ -0,0 +1,16 @@
+﻿<?php
+if(isset($_POST['submit']))
+{
+str=strtolower($_POST['str']);
+$vowels=array('a','e','i','o','u');
+$len=strlen($str);
+$num=0;
+for($i=0;$i<$len;$i++)
+{
+if(in_array($str[$i],$vowels))
+{$num++;
+}
+}
+echo "Number of vowels:$num";
+}
+?>
---
+++ projects/PROJECTVIP/sop12.php
@@ -0,0 +1,12 @@
+﻿<?php>
+$a=array(60,78,74,85,96);
+$t=0;
+foreach($a as $value)
+{
+echo "<br> Marks in subject”.$value;
+$t=$t+$value;
+}
+$p=$t*100/500;
+echo <br>"Total is :”.$t;
+echo<br> “Percentage is :”.$p;
+?>
